a5dccdf26b61aa985f7f662207b9db3ccdeaf260
itzmaryamxo <246maryamao@gmail.com>
2025-06-28 10:15:30 +0300

    first commit

---
+++ ChatApp/ChatApp/asgi_application.py
@@ -0,0 +1,13 @@
+import os
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatApp.settings")
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # Just HTTP for now. (We can add other protocols later.)
+})
+
+ASGI_APPLICATION = 'ChatApp.asgi.application'
